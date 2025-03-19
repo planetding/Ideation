701910f6f7507611c8370d8bbe0769235fa5629a
planetding <planetding@gmail.com>
2025-03-19 18:57:43 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,7 @@
 import { useState, useEffect } from 'react';
 import { createClient } from '@supabase/supabase-js';
 
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-);
+
 
 const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL || '', process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
 
