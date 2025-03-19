144dc9e268a7eb72ea352bf5bae785a25c70d8f1
planetding <planetding@gmail.com>
2025-03-19 19:02:02 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { createClient } from '@supabase/supabase-js';
 
 
 
-const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL || '', process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL as string, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string );
 
 export default function Home() {
   const [user, setUser] = useState(null);
